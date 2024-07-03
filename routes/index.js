185c4ba5e95918a218606ca6eca4ee565e42ac92
tolu13 <tolulopeolanrewaju8@gmail.com>
2024-07-03 15:59:32 -0700

    cool

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
 function routing(app) {
   const router = express.Router();
