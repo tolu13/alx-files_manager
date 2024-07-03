dde8133b0360e1039264c5a1d88c9ce9bf024aa8
tolu13 <tolulopeolanrewaju8@gmail.com>
2024-07-03 05:29:57 -0700

    updated to pass linter error

--- utils/redis.js
+++ utils/redis.js
@@ -2,33 +2,33 @@ import redis from 'redis';
 import { promisify } from 'util';
 
 class RedisClient {
-    constructor() {
-        this.client = redis.createClient();
+  constructor() {
+    this.client = redis.createClient();
 
-        // Handle Redis client errors
-        this.client.on('error', (error) => {
-            console.error('Redis client error:', error);
-        });
-    }
+    // Handle Redis client errors
+    this.client.on('error', (error) => {
+      console.error('Redis client error:', error);
+    });
+  }
 
-    isAlive() {
-        return this.client.connected;
-    }
+  isAlive() {
+    return this.client.connected;
+  }
 
-    async get(key) {
-        const getAsync = promisify(this.client.get).bind(this.client);
-        return await getAsync(key);
-    }
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+    return getAsync(key);
+  }
 
-    async set(key, value, durationInSeconds) {
-        const setAsync = promisify(this.client.set).bind(this.client);
-        return await setAsync(key, value, 'EX', durationInSeconds);
-    }
+  async set(key, value, durationInSeconds) {
+    const setAsync = promisify(this.client.set).bind(this.client);
+    return setAsync(key, value, 'EX', durationInSeconds);
+  }
 
-    async del(key) {
-        const delAsync = promisify(this.client.del).bind(this.client);
-        return await delAsync(key);
-    }
+  async del(key) {
+    const delAsync = promisify(this.client.del).bind(this.client);
+    return delAsync(key);
+  }
 }
 
 // Create and export a singleton instance of RedisClient
